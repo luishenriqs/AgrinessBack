d51e07f4457f75adaf92747db49f7f3fbc1a3234
Luís Henrique Pereira <lh.p@hotmail.com>
2021-10-21 08:29:33 -0300

    Saving and unlink file

--- src/modules/animals/useCases/importAnimals/ImportAnimalsUseCase.ts
+++ src/modules/animals/useCases/importAnimals/ImportAnimalsUseCase.ts
@@ -1,15 +1,17 @@
 import fs from 'fs';
+import { Animal } from '../../model/Animal';
 import { IAnimalsRepository } from "../../repositories/IAnimalsRepository";
 
 class ImportAnimalsUseCase {
     constructor(private animalsRepository: IAnimalsRepository) {};
 
     loadingFile = (file: Express.Multer.File, encoding) => {
-        return new Promise((resolve, reject) => {
+        return new Promise<Animal[]>((resolve, reject) => {
             fs.readFile(file.path, encoding = "utf-8", function read(err, data) {
                 if (err) {
                     reject(err);
                 }
+                fs.promises.unlink(file.path);
                 resolve(JSON.parse(data));
             });
 
@@ -18,7 +20,42 @@ class ImportAnimalsUseCase {
 
     async execute(file: Express.Multer.File, encoding): Promise<void> {
         const animals = await this.loadingFile(file, encoding);
-        console.log('----------> Array de animais ', animals[0]);
+
+        animals.map(async (animal) => {
+            const {
+                id,
+                nome,
+                tipoAnimal,
+                statusAnimal,
+                localizacao,
+                dataNascimento,
+                entradaPlantel,
+                pesoCompra,
+                raca,
+                codigoRastreamento,
+                faseProducao,
+                tipoGranja,
+            } = animal;
+
+            const existAnimal = this.animalsRepository.findByName(nome);
+
+            if(!existAnimal) {
+                this.animalsRepository.create({
+                    id,
+                    nome,
+                    tipoAnimal,
+                    statusAnimal,
+                    localizacao,
+                    dataNascimento,
+                    entradaPlantel,
+                    pesoCompra,
+                    raca,
+                    codigoRastreamento,
+                    faseProducao,
+                    tipoGranja,
+                });
+            };
+        });
     };
 };
 
