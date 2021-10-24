4e3fbac0d1e984dbaa028a58d9a7934e83f72910
Luís Henrique Pereira <lh.p@hotmail.com>
2021-10-24 20:28:15 -0300

    Implementing documentation with the swagger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
 import { router } from './routes';
 import './database';
 import './shared/container';
 import { AppError } from './errors/AppError';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
