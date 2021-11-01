1738d89aea3ac80e17571f8cb5a8c9a0438d7fc8
Luís Henrique Pereira <lh.p@hotmail.com>
2021-11-01 11:22:57 -0300

    FindIdUseCase complete

--- src/modules/animals/useCases/findId/FindIdUseCase.ts
+++ src/modules/animals/useCases/findId/FindIdUseCase.ts
@@ -1,16 +1,50 @@
 import "reflect-metadata"
 import { inject, injectable } from 'tsyringe';
-import { Animal } from "../../entities/Animal";
 import { IAnimalsRepository } from "../../repositories/IAnimalsRepository";
+import { IFaseProducaoRepository } from "../../repositories/IFaseProducaoRepository";
+import { ITipoGranjaRepository } from "../../repositories/ITipoGranjaRepository";
 
+interface IAnimalReturn {
+    id?: String;
+    nome: String;
+    tipoAnimal: String;
+    statusAnimal: String;
+    localizacao: String;
+    dataNascimento: String;
+    entradaPlantel: String;
+    pesoCompra: String;
+    raca: String;
+    codigoRastreamento: String;
+    faseProducao: {
+        id?: string;
+        sigla: string;
+        descricao: string;
+    };
+    tipoGranja: {
+        id?: string;
+        sigla: string;
+        descricao: string;
+    };
+};
 @injectable()
 class FindIdUseCase {
     constructor(
         @inject("AnimalsRepository")
         private animalsRepository: IAnimalsRepository,
+
+        @inject("FaseProducaoRepository")
+        private faseProducaoRepository: IFaseProducaoRepository,
+
+        @inject("TipoGranjaRepository")
+        private tipoGranjaRepository: ITipoGranjaRepository,
     ) {};
-    async execute({ id }): Promise<Animal> {
+    async execute({ id }): Promise<IAnimalReturn> {
         let animal = await this.animalsRepository.findById(id);
+        let faseProducao = await this.faseProducaoRepository.findById(id);
+        let tipoGranja = await this.tipoGranjaRepository.findById(id);
+
+        animal = { ...animal, faseProducao, tipoGranja }
+
         return animal;
     }
 };
